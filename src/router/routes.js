d08303add418803c2372614e63b1e83429f61fd4
kcarbzz <rakion2r@gmail.com>
2025-10-31 21:12:43 -0500

    sprint 1 login

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,18 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'oink', component: () => import('src/components/auth/RegisterForm.vue') },
+    ],
+  },
+  {
+    path: '/login',
+    component: () => import('src/components/auth/LoginForm.vue'),
+  },
+  {
+    path: '/register',
+    component: () => import('src/components/auth/RegisterForm.vue'),
   },
 
   // Always leave this as last one,
